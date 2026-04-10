723f62ca4e62daf882f9728649a927c9d9c41840
Adityakumar41347 <adityakumar41347@gmail.com>
2026-04-10 16:11:27 +0530

    Separated backend directory

--- backend/src/models/index.ts
+++
@@ -1,8 +0,0 @@
-export { Crop }             from "./Crop";
-export { Vehicle }          from "./Vehicle";
-export { Mandi }            from "./Mandi";
-export { MarketPrice }      from "./MarketPrice";
-export { Farmer }           from "./Farmer";
-export { Trip }             from "./Trip";
-export { PriceAlert }       from "./PriceAlert";
-export { RideshareRequest } from "./RideshareRequest";
